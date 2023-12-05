09f0cbc173cb6eb3cd4d75ad51ca1f3eaa80d77e
ChaoticFS <104865518+ChaoticFS@users.noreply.github.com>
2023-12-05 07:29:04 +0100

    Added test for login endpoint, should make more for further coverage

--- AuthServiceTests/AuthTests.cs
+++ AuthServiceTests/AuthTests.cs
@@ -1,16 +1,71 @@
+using Moq;
+using AuthService.Repositories;
+using AuthService.Controllers;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using AuthService.Models;
+using Microsoft.AspNetCore.Mvc;
+
 namespace AuthServiceTests
 {
+    [TestFixture]
     public class AuthTests
     {
+        private Mock<IAuthRepository> _authRepositoryStub;
+        private AuthController _authController;
+
         [SetUp]
         public void Setup()
         {
+            _authRepositoryStub = new Mock<IAuthRepository>();
+            var loggerStub = new Mock<ILogger<AuthController>>();
+            var configurationStub = new Mock<IConfiguration>();
+
+            _authController = new AuthController(loggerStub.Object, configurationStub.Object, _authRepositoryStub.Object);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _authController.Dispose();
         }
 
         [Test]
-        public void Test1()
+        [TestCase("John", "Doe", true)]
+        [TestCase("Jane", "Deere", false)]
+        public async Task LoginReturnsJwt(string username, string password, bool expectedResult)
         {
-            Assert.Pass();
+            // arrange
+            AuthDTO authDTO = new AuthDTO { Username = username, Password = password };
+
+            if (expectedResult)
+            {
+                _authRepositoryStub.Setup(repo => repo.Login(authDTO)).ReturnsAsync("dummyToken");
+            }
+            else
+            {
+                _authRepositoryStub.Setup(repo => repo.Login(authDTO)).ReturnsAsync((string)null);
+            }
+
+            // act
+            var result = await _authController.Login(authDTO);
+
+            // assert
+            if (expectedResult)
+            {
+                Assert.IsInstanceOf<OkObjectResult>(result);
+                var okResult = (OkObjectResult)result;
+
+                Assert.AreEqual(200, okResult.StatusCode);
+                Assert.IsInstanceOf<string>(okResult.Value);
+            }
+            else
+            {
+                Assert.IsInstanceOf<BadRequestObjectResult>(result);
+                var badResult = (BadRequestObjectResult)result;
+
+                Assert.AreEqual(400, badResult.StatusCode);
+            }
         }
     }
 }
