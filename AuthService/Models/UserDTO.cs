5e7646ac0ae993d109483ecb676f16166ac79508
ChaoticFS <104865518+ChaoticFS@users.noreply.github.com>
2023-12-06 09:34:32 +0100

    schema.org compliance

--- AuthService/Models/UserDTO.cs
+++ AuthService/Models/UserDTO.cs
@@ -6,7 +6,7 @@
         public string? Username { get; set; }
         public string? Password { get; set; }
         public string? FirstName { get; set; }
-        public string? LastName { get; set; }
+        public string? Surname { get; set; }
         public int? Phone { get; set; }
         public string? Email { get; set; }
         public string? Address { get; set; }
