91fe575e75472634690c53b1fcd08cd655309bcb
ChaoticFS <Magnus.overgaard1711@gmail.com>
2023-12-14 14:05:10 +0100

    updated user with new field

--- AuthService/Models/UserDTO.cs
+++ AuthService/Models/UserDTO.cs
@@ -10,5 +10,6 @@
         public int? Phone { get; set; }
         public string? Email { get; set; }
         public string? Address { get; set; }
+        public DateTime RegistrationDate { get; set; }
     }
 }
