2f6d32b805fde7e5ee264b9070e15869ff2172e3
“browndahl” <mrbrowndahl@gmail.com>
2023-12-15 09:02:34 +0100

    GUID ADDED

--- AuthService/Models/UserDTO.cs
+++ AuthService/Models/UserDTO.cs
@@ -2,7 +2,9 @@
 {
     public class UserDTO
     {
-        public int? UserId { get; set; }
+
+        
+        public Guid? UserId { get; set; }
         public string? Username { get; set; }
         public string? Password { get; set; }
         public string? FirstName { get; set; }
